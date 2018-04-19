40245dc55aa0cba2d0e8e0cce13bd5984aa2d375
Luis07Hernandez <36206133+Luis07Hernandez@users.noreply.github.com>
2018-04-18 23:55:24 -0500

    Tarea
    Estructura con arreglos

---
+++ Catalogo.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EstructurasConArreglos
+{
+    class Catalogo
+    {
+        private string[] vec;
+        private string Informacion;
+        private int contador;
+
+   
+
+        public Catalogo()
+        {
+            Informacion = "";
+            vec = new string[15];
+            contador = 0;
+        }
+
+      /*  public bool validarAgregar(string placa)
+        {
+            for (int x = 0; x < contador; x++)
+            {
+                if (vec[x].Contains(placa))
+                {
+                    return true;
+                }
+                else
+                    return false;
+            }
+
+            return false;
+        }*/
+
+        public void agregar(string nombre1, string año1, string placa1, string telefono1)
+        {
+            
+            Informacion = " ";
+            Informacion = nombre1 + " " + año1 + " " + placa1 + " " + telefono1 + " ";
+            vec[contador] = Informacion;
+            contador++;
+        }
+
+        public string listar()
+        {
+            string lista= "";
+            for (int x = 0; x < contador; x++)
+            {
+               lista += vec[x] + Environment.NewLine;
+              
+            }
+            return lista;
+        }
+
+        public void eliminar(string placa)
+        {
+            for (int x = 0; x < contador; x++)
+            {
+                if (vec[x].Contains(placa))
+                {
+
+                    while (x < contador)
+                    {
+                        vec[x] = vec[x + 1];
+                        x++;
+                    }                 
+            
+                }
+
+            }
+            contador--;
+        }
+
+        public void insertar(string nombre1, string año1, string placa1, string telefono1, string placa)
+        {
+            for (int x = 0; x < contador; x++)
+            {
+                if (vec[x].Contains(placa))
+                {
+                    Informacion = " ";
+                    Informacion = nombre1 + " " + año1 + " " + placa1 + " " + telefono1 + " ";
+                    vec[x] = Informacion;
+                }
+
+            }
+
+        }
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EstructurasConArreglos
+{
+    public partial class Form1 : Form
+    {
+        Catalogo obj = new Catalogo();
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            
+                obj.agregar(txtNombre.Text, txtAño.Text, txtPlaca.Text, txtTelefono.Text);
+            
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            txtResultado.Clear();
+            txtResultado.Text = obj.listar();
+            
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            obj.eliminar(txtBPlaca.Text);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            obj.insertar(txtNombre.Text, txtAño.Text, txtPlaca.Text, txtTelefono.Text, txtBPlaca.Text);
+        }
+    }
+}
